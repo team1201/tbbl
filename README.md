a5f36ecde4fe2a7c86767fb0e21fe86fd7295898
zrong <zrongzrong@gmail.com>
2015-04-17 14:09:02 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+TEAM1201 Builder Basement Library
+===================================
+
+- 作者： [zrong](http://zengrong.net)
+- 创建： 2015-04-17
+
+关于 TEAM1201 Builder 在这里有介绍： <http://doc.zengrong.net/1201/project-doc/>
+
+对于采用 cocos2d-x 的每个项目，都有一个 builder 工具进行配合。
+
+这些 builder 的大部分功能是一致的，因此我希望将这些一致的功能整合在一起，而不是每次更新都要更新多次。
+
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+requires = []
+
+dependency_links = [
+    'http://192.168.18.18/project/hhl/tool/zrong-0.2.2.tar.gz'
+]
+
+entry_points = {
+    'console_scripts': [
+        'xlc = xlc:call',
+    ]
+}
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+package_data = {
+    'hhlc':['build.conf'],
+}
+
+#data_files = [(root, [os.path.join(root, f) for f in files])
+#    for root, dirs, files in os.walk('bin')]
+
+setup(
+    name="tbbl",
+    version="0.0.1",
+    url='http://zengrong.net/',
+    author='zrong',
+    author_email='zrongzrong@gmail.com',
+    description="A basement library for TEAM1201's builder tools.",
+    packages=['tbbl'],
+    classifiers=classifiers,
+    #include_package_data=True,
+    #package_data=package_data,
+    #data_files=data_files,
+    #install_requires=requires,
+    #entry_points=entry_points,
+    #dependency_links = dependency_links,
+    #test_suite='hhlb.test',
+)
